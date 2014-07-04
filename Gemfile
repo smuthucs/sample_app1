43187ced0b45dd218be8d0e92550d1b7a0c52b68
Muthukumar Subramaniam <smuthukumarcs@gmail.com>
2014-07-05 01:36:20 +0530

    Finished static pages

--- Gemfile
+++ Gemfile
@@ -12,6 +12,9 @@ end
 group :test do
   gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
+  gem 'spork', '>0.9.0.rc'
+  gem 'guard-rspec'
+  gem 'guard-spork'
 end
 
 gem 'sass-rails', '4.0.1'
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,16 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title 'Home'" do
+      visit '/pages/home'
+      expect(page).to have_title('Home')
     end
   end
 
@@ -14,6 +19,26 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title 'Home'" do
+      visit '/pages/contact'
+      expect(page).to have_title('Contact')
+    end
+
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title 'Home'" do
+      visit '/pages/about'
+      expect(page).to have_title('About')
+    end
+
   end
 
 end
+
